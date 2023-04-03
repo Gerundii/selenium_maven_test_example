502c5df29ab322c26255b2509877c6298f2ae7ea
lig-tests-repo <37630665+autotest-lessons@users.noreply.github.com>
2023-04-03 18:54:24 +0300

    Update FirstTest.java

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 
 public class FirstTest {
@@ -9,8 +12,9 @@ public class FirstTest {
     @Test
     public void testGoogle() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("Selenium Test");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.google.ru/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q"))).sendKeys("Selenium Test");
         driver.findElement(By.name("btnK")).click();
     }
 
