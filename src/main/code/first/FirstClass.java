f16e6aa0bb73805be1bc92727a5e423402fee9f9
mtekuchev <Irz=Igb!5b>
2023-04-15 22:42:25 +0300

    feat: add sixthHomeTask

---
+++ src/main/code/first/FirstClass.java
@@ -0,0 +1,9 @@
+package code.first;
+import code.second.TwoClass;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Автотесты интересны");
+        System.out.println(TwoClass.sumTwoInt(4,5));
+    }
+}
---
+++ src/main/code/second/TwoClass.java
@@ -0,0 +1,8 @@
+package code.second;
+
+public class TwoClass {
+    public static  int sumTwoInt(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+}
