b869b9d4775c6f88204544909ca8f4e7a7bb86fe
mtekuchev <Irz=Igb!5b>
2023-04-20 21:52:56 +0300

    feat: add HomeTask7

---
+++ src/main/code/HomeTask7.java
@@ -0,0 +1,16 @@
+public class HomeTask7 {
+    public static void main(String[] args) {
+        PI();
+        num();
+    }
+
+    private static void PI(){
+        final double PI = 3.14;
+        System.out.println("Значение числа π равно" + " " + PI);
+    }
+
+    private static void num(){
+        int num = 2 * 5;
+        System.out.println("Искомое число равно" + " " + num);
+    }
+}
